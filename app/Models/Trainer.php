6c49d36722dcaf3a393aa90c16c2f8c43b3960e9
zxSHOXxz <lalameatlala123@gmail.com>
2023-06-01 23:26:08 +0300

    loginAuth

--- app/Models/Trainer.php
+++ app/Models/Trainer.php
@@ -3,9 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Trainer extends Model
+class Trainer extends Authenticatable
 {
     use HasFactory;
 
